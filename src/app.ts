a82d753fbf0bc76a2707aa23d9d785a912d7688c
Dimitar Malinov <malinovd02@gmail.com>
2023-01-03 12:50:17 +0200

    step 1: Setup application

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('working?')
+import * as PIXI from 'pixi.js';
+
+const app = new PIXI.Application({
+    width: 800,
+    height: 600,
+});
+
+document.body.appendChild(app.view as HTMLCanvasElement);
+
+app.ticker.add(update);
+
+function update(dt: number) {
+
+}
