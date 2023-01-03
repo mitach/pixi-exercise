85d22a6c283121c09c42e2c63fcdab6fb38b92ad
Dimitar Malinov <malinovd02@gmail.com>
2023-01-03 13:08:28 +0200

    step 3: Create googly eyes

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,30 @@ background.beginFill(0xcccccc);
 background.drawRect(0, 0, 800, 600);
 background.endFill();
 
+const eyes = new PIXI.Container();
+
+const sclera = new PIXI.Graphics();
+sclera.beginFill(0xffffff);
+sclera.drawCircle(10, 10, 7);
+sclera.drawCircle(10, 30, 7);
+sclera.endFill();
+
+const leftPupil = new PIXI.Graphics();
+leftPupil.beginFill(0x000000);
+leftPupil.drawCircle(10, 10, 5);
+leftPupil.endFill();
+
+const rightPupil = new PIXI.Graphics();
+rightPupil.beginFill(0x000000);
+rightPupil.drawCircle(10, 30, 5);
+rightPupil.endFill();
+
+eyes.addChild(sclera);
+eyes.addChild(leftPupil);
+eyes.addChild(rightPupil);
+
 app.stage.addChild(background);
+app.stage.addChild(eyes);
 
 app.ticker.add(update);
 
