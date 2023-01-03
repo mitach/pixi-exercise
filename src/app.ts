a5bd2bdee283471ba3531c385eafe97ad8760813
Dimitar Malinov <malinovd02@gmail.com>
2023-01-03 13:46:13 +0200

    step 5: Create bug legs

--- src/app.ts
+++ src/app.ts
@@ -34,8 +34,53 @@ eyes.addChild(sclera);
 eyes.addChild(leftPupil);
 eyes.addChild(rightPupil);
 
+const bug = new PIXI.Container();
+
+const legTexture = PIXI.Texture.from('assets/leg.png');
+legTexture.defaultAnchor.set(1, 0.25);
+
+const legs = [
+    new PIXI.Sprite(legTexture),
+    new PIXI.Sprite(legTexture),
+    new PIXI.Sprite(legTexture),
+    new PIXI.Sprite(legTexture),
+    new PIXI.Sprite(legTexture),
+    new PIXI.Sprite(legTexture),
+];
+
+for (let leg = 0; leg < legs.length; leg++) {
+    legs[leg].rotation = Math.PI / 2;
+
+    if (leg == 0 || leg == 1 || leg == 2) {
+        legs[leg].scale.set(0.3, 0.3);
+    } else if (leg == 3 || leg == 4 || leg == 5) {
+        legs[leg].scale.set(-0.3, 0.3);
+    }
+
+    if (leg == 0) {
+        legs[leg].position.set(-25, -20);
+    } else if (leg == 1) {
+        legs[leg].position.set(-10, -20);
+    } else if (leg == 2) {
+        legs[leg].position.set(5, -15);
+    } else if (leg == 3) {
+        legs[leg].position.set(-25, 20);
+    } else if (leg == 4) {
+        legs[leg].position.set(-10, 20);
+    } else if (leg == 5) {
+        legs[leg].position.set(5, 15);
+    }
+}
+
+for (let leg of legs) {
+    bug.addChild(leg);
+}
+
+bug.position.set(100);
+
 app.stage.addChild(background);
 app.stage.addChild(eyes);
+app.stage.addChild(bug);
 
 app.ticker.add(update);
 
